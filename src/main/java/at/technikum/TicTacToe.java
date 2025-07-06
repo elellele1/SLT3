a90e2c8a253666fd17dde7b41546e0e743b1fd30
elellele1 <wi24b072@technikum-wien.at>
2025-07-06 23:44:49 +0200

    feat: Spielfeld ausgeben hinzugefügt

--- src/main/java/at/technikum/TicTacToe.java
+++ src/main/java/at/technikum/TicTacToe.java
@@ -27,5 +27,22 @@ public class TicTacToe {
                 System.out.println("---+---+---");
             }
         }
+
+        public void printBoard() {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    System.out.print(" " + board[i][j]);
+                    if (j < board[i].length - 1) {
+                        System.out.print(" |");
+                    }
+                }
+                System.out.println();
+                if (i < board.length - 1) {
+                    System.out.println("---+---+---");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
