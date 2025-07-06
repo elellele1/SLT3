4e2f3819528d4b7c3f934c5918ac1bac79255d99
elellele1 <wi24b072@technikum-wien.at>
2025-07-06 23:57:02 +0200

    feat: Spielfeld initialisiert und Grundstruktur erstellt

--- src/main/java/at/technikum/TicTacToe.java
+++ src/main/java/at/technikum/TicTacToe.java
@@ -13,65 +13,17 @@ public class TicTacToe {
     public void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+                board[i][j] = ' ';
             }
         }
-    }
-
-    public void printBoard() {
-        System.out.println("Spielbrett:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public boolean placeMove(int row, int col) {
-        if (board[row][col] == '-') {
-            board[row][col] = currentPlayer;
-            return true;
-        }
-        return false;
-    }
-
-    public void changePlayer() {
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
-
-    public boolean checkWin() {
-        // Horizontal, Vertikal und Diagonal überprüfen
-        for (int i = 0; i < 3; i++) {
-            if ((board[i][0] == currentPlayer &&
-                    board[i][1] == currentPlayer &&
-                    board[i][2] == currentPlayer) ||
-                    (board[0][i] == currentPlayer &&
-                            board[1][i] == currentPlayer &&
-                            board[2][i] == currentPlayer)) {
-                return true;
-            }
-        }
-        return (board[0][0] == currentPlayer &&
-                board[1][1] == currentPlayer &&
-                board[2][2] == currentPlayer) ||
-                (board[0][2] == currentPlayer &&
-                        board[1][1] == currentPlayer &&
-                        board[2][0] == currentPlayer);
-    }
-
-    public boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 
     public char getCurrentPlayer() {
         return currentPlayer;
     }
+
+    public char[][] getBoard() {
+        return board;
+    }
 }
+
