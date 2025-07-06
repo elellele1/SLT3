e7a2c63661eb0609374ed3020e7bcd2ba564661b
elellele1 <wi24b072@technikum-wien.at>
2025-07-07 00:10:46 +0200

    feat: finale Version TicTacToe & Main eingefügt

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -1,16 +1,27 @@
 package at.technikum;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe(); // Spielobjekt erstellen
-        game.printBoard(); // Board ausgeben
-    }
+        TicTacToe game = new TicTacToe();
+        Scanner scanner = new Scanner(System.in);
 
-    public void printBoard() {
-        // Spielfeld wird schön formatiert ausgegeben
+        System.out.println("Willkommen bei Tic Tac Toe!");
+        game.printBoard();
+
+        while (!game.isGameOver()) {
+            System.out.println("Spieler " + game.getCurrentPlayer() + ", gib deine Position ein (Zeile und Spalte von 0 bis 2): ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if (game.makeMove(row, col)) {
+                game.printBoard();
+            } else {
+                System.out.println("Ungültiger Zug, versuch es nochmal.");
+            }
+        }
+
+        System.out.println("Spiel beendet!");
     }
 }
-
-
-
