4b7eb78a2618a0ac3d11fecdcc81fd83c59cc9ac
elellele1 <wi24b072@technikum-wien.at>
2025-07-06 23:34:34 +0200

    feat: Spielfeldanzeige in printBoard hinzugefügt

--- src/main/java/at/technikum/TicTacToe.java
+++ src/main/java/at/technikum/TicTacToe.java
@@ -1,4 +1,32 @@
 package at.technikum;
 
 public class TicTacToe {
+
+    private char[][] board;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("Aktueller Spielstand:");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(" " + board[i][j]);
+                if (j < board[i].length - 1) {
+                    System.out.print(" |");
+                }
+            }
+            System.out.println();
+            if (i < board.length - 1) {
+                System.out.println("---+---+---");
+            }
+        }
+    }
 }
+
